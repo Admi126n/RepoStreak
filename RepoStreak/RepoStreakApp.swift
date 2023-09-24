8c7edc1b462b5ba88bc9d23442ef164f5fa7a4a4
Adam Tokarski <a.tokarski126@gmail.com>
2023-09-24 14:55:43 +0200

    Initial Commit

---
+++ RepoStreak/RepoStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoStreakApp.swift
+//  RepoStreak
+//
+//  Created by Adam Tokarski on 24/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RepoStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
